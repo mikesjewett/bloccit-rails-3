2b65d0c4c3fd8538a846d53cd28ae48a1153bc5e
Mike Jewett <mikesjewett@gmail.com>
2013-05-16 15:53:28 -0400

    Associated and nested posts and topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'faker'
 
+# Create 15 topics
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
+ 
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -11,24 +21,51 @@ rand(4..10).times do
   u.save
 
   rand(5..12).times do
+    topic = topics.first
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
-    rand(3..7).times do
-      p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-    end
+    topics.rotate!
   end
 end
 
-u = User.first
-u.update_attributes(email: 'mike@bloc.io', password: 'helloworld', password_confirmation: 'helloworld')
-u.confirm!
+post_count = Post.count
+User.all.each do |user|
+    rand(30..50).times do
+      p = Post.find(rand(1..post_count))
+    end
+end
+
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
