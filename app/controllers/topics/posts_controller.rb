d962aaa715373ee7d2d8f903b9d91d81a61753d3
Mike Jewett <mikesjewett@gmail.com>
2013-06-04 07:12:23 -0700

    Removed local vote variable

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -5,6 +5,7 @@ class Topics::PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
     @comment = Comment.new
+    @vote = current_user.voted(@post)
   end
 
   def new
