dc5b34c2f2ffe262be8143f77a71d360c3454f57
Mike Jewett <mikesjewett@gmail.com>
2013-06-04 06:55:35 -0700

    Created Popular posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
   def show
     @user = User.find(params[:id])  
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
+
   get "comments/create"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
   resources :users, only: [:show]
   
+  resources :posts, only: [:index]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
