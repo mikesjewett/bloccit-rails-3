e6f2b128d1db6ff202f6f168161fe6b154eb10a5
Mike Jewett <mikesjewett@gmail.com>
2013-09-13 16:17:27 -0400

    renamed public scope to visible_to

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ActiveRecord::Base
   attr_accessible :description, :name, :public
   has_many :posts, dependent: :destroy
 
-  scope :public, lambda { |user| user ? scoped : where(public: true) }
+  scope :visible_to, lambda { |user| user ? scoped : where(public: true) }
 end
